ed1d7cc2d6e313f0ee6103a215a8c563a592768f
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-09-25 23:45:13 +0300

    MenuActivity added

--- app/src/main/java/com/example/readed/MenuActivity.java
+++ app/src/main/java/com/example/readed/MenuActivity.java
@@ -31,16 +31,7 @@ public class MenuActivity extends AppCompatActivity {
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
-        ChipNavigationBar tabs = findViewById(R.id.tabs);
+        TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
